4651e59e9a2dcb2a5eecee3a36561cc62fb92969
Lewis <tinglewis633@gmail.com>
2021-06-09 07:07:32 +0000

    add Appointment  and Interview component

--- src/components/Appointment.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default function Appointment(props) {
-  return <section>hi</section>;
-}
-  
---
+++ src/components/Appointment/index.js
@@ -0,0 +1,6 @@
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);
